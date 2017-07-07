cf207d822cc385124a46a9285801f4c18cee05d1
Átila Mendes da Silva Tosta <atila.tosta@hotmail.com>
2017-07-07 14:45:10 -0300

    Initial Commit

---
+++ wdc_stt_example/Program.cs
@@ -0,0 +1,40 @@
+﻿using IBM.WatsonDeveloperCloud.SpeechToText.v1;
+using IBM.WatsonDeveloperCloud.SpeechToText.v1.Model;
+using IBM.WatsonDeveloperCloud.SpeechToText.v1.Util;
+using System;
+using System.IO;
+
+namespace wdc_stt_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISpeechToTextService _speechToText = new SpeechToTextService("0c8dafce-3cf2-46d8-9116-408e35a35fe2", "MaiEL4fS1zlJ");
+
+            using (FileStream fs = File.OpenRead("test-audio.wav"))
+            {
+                Console.WriteLine("\nCalling RecognizeBody...");
+                var speechEvent = _speechToText.Recognize(fs.GetMediaTypeFromFile(),
+                                                          fs);
+
+                Console.WriteLine("speechEvent received...");
+                if (speechEvent.Results != null || speechEvent.Results.Count > 0)
+                {
+                    foreach (SpeechRecognitionResult result in speechEvent.Results)
+                    {
+                        if (result.Alternatives != null && result.Alternatives.Count > 0)
+                        {
+                            foreach (SpeechRecognitionAlternative alternative in result.Alternatives)
+                            {
+                                Console.WriteLine(string.Format("{0}, {1}", alternative.Transcript, alternative.Confidence));
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
